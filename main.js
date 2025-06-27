113d57da8746773d49305fd356037286106452de
Mfahad159 <mianfahad664@gmail.com>
2025-06-27 09:11:13 +0500

    Updated main

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 console.log("\n  ( * q * )");
 
+ function getRandomColor() {
+            const letters = '0123456789ABCDEF';
+            let color = '#';
+            for (let i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
 
-
+            return color;
+        }
+        let intervalId;
+        document.querySelector('#start').addEventListener('click', () => {
+          if(!intervalId){
+            intervalId =  setInterval(() => {
+             
+                document.body.style.backgroundColor = getRandomColor();
+            }, 1000);
+          }
+        });
+        document.querySelector('#stop').addEventListener('click', () => {
+            clearInterval(intervalId);
+            intervalId = null;
+        });
