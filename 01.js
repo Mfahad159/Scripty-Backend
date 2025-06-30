0d3facdafadb229db1b2ae2115ee145961c6201b
Mfahad159 <mianfahad664@gmail.com>
2025-06-30 07:47:20 +0500

    Added fetch() in 01.js

--- 01.js
+++ 01.js
@@ -1,16 +1,27 @@
-new Promise((resolve,reject)=>{
-    if(false){
-    setTimeout(()=>{
-        console.log("Hello, World!");
-    }, 1000);
-    resolve("Done!");
-    }
-    else{
-    reject("Error: Condition is false.");
-    }
+// new Promise((resolve,reject)=>{
+//     if(false){
+//     setTimeout(()=>{
+//         console.log("Hello, World!");
+//     }, 1000);
+//     resolve("Done!");
+//     }
+//     else{
+//     reject("Error: Condition is false.");
+//     }
 
-}).then((message) => {
-    console.log(message);
-}).catch((error) => {
-    console.error(error);
-});
+// }).then((message) => {
+//     console.log(message);
+// }).catch((error) => {
+//     console.error(error);
+// });
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+   return response.json();
+})
+.then((data) => {
+ console.log(data);
+})
+.catch((error) => {
+   console.error('Error fetching data:', error);
+})
